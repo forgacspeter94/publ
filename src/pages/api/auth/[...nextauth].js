1a22dbbe0e4cc676de887e1280b27b9ad4060c52
Mate Mark Csornyei <mate@csornyei.com>
2023-05-03 22:27:34 +0200

    Basic setup

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/utils/db";
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+};
+export default NextAuth(authOptions);
